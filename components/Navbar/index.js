35477425dc6d016b80807163d5da99ce371ad4e8
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-22 21:39:53 -0400

    Created navbar and navbar folder

---
+++ components/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from "./node_modules/react"
+
+function Navbar() {
+    return (
+        <nav class="navbar navbar-light bg-light">
+  <span class="navbar-brand mb-0 h1">REACT Employee Directory</span>
+</nav>
+    )
+}
+
+export default Navbar 
