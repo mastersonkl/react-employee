dd6c10298472be5234b2e0bb17bd3892683aa6ef
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-23 11:38:08 -0400

    Changed table function

--- components/table/index.js
+++ components/table/index.js
@@ -1,55 +1,18 @@
 import React from "react"
 
 function Table(props) {
-    return(
+    return (
         <table className="table">
-  <thead className="thead-light">
-    <tr>
-      <th scope="col"></th>
-      <th scope="col">First Name</th>
-      <th scope="col">Last Name</th>
-      <th scope="col">Email</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Amanda</td>
-      <td>Mahon</td>
-      <td>amanda@gmail.com</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Emily</td>
-      <td>Burke</td>
-      <td>emily@gmail.com</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Morgan</td>
-      <td>Vaughan</td>
-      <td>morgan@gmail.com</td>
-    </tr>
-    <tr>
-      <th scope="row">4</th>
-      <td>Allison</td>
-      <td>Fitzgerald</td>
-      <td>allison@gmail.com</td>
-    </tr>
-    <tr>
-      <th scope="row">5</th>
-      <td>Peyton</td>
-      <td>Allison</td>
-      <td>peyton@gmail.com</td>
-    </tr>
-    <tr>
-      <th scope="row">6</th>
-      <td>Kendall</td>
-      <td>Masterson</td>
-      <td>kendall@gmail.com</td>
-    </tr>
-  </tbody>
-</table>
+            <thead className="thead-light">
+                <tr>
+                    <th scope="col"></th>
+                    <th scope="col">{props.firstHeader}</th>
+                    <th scope="col">{props.lastHeader}</th>
+                    <th scope="col">{props.emailHeader}</th>
+                </tr>
+            </thead>
+            <Searchresults results={props.results} />
+        </table>
     )
 }
 
