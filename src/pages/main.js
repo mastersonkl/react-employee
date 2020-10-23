545eac63f295525c854ec23ac1fe516fcb04716b
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-23 12:18:13 -0400

    Updated API call

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,12 +1,46 @@
-import React, { Component, useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import Searchbar from "../components/searchbar"
+import API from "../utils/api"
 import Table from "../components/table"
 
 
 function Main() {
-    const [employee, setEmployee] = useState({})
     const [employees, setEmployees] = useState([])
-    const [employeeIndex, setEmployeeIndex] = useState(0)
+
+
+    useEffect(() => {
+        loadEmployees()
+    }, [])
+
+    function loadEmployees() {
+        API.fetchEmployees()
+            .then(res => {
+                console.log("API res", res.data.results)
+                const APIResults = res.data.results
+                const results = APIResults.map(employee => {
+                    return {
+                        image: employee.picture.thumbnail,
+                        firstName: employee.name.first,
+                        lastName: employee.name.last,
+                        email: employee.email
+                    }
+                })
+                console.log("Employee Data", results)
+                setEmployees(results)
+            })
+            .catch(err => console.log(err))
+    }
+
+    return (
+        <div>
+            <Searchbar />
+            <Table results={employees} />
+        </div>
+    )
 }
+
+
+
+
 
 export default Main 
