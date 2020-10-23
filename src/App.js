0a1aee80390c63db7eb66887e38c6ff9e37a6a37
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-23 10:46:11 -0400

    Added wrapper and table

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom"
-import Navbar from "./componets/Navbar"
-import Searchbar from './componets/searchbar';
+import Navbar from "./components/navbar"
+import Searchbar from './components/searchbar'
+import Wrapper from "./components/wrapper"
+import Table from "./components/table"
 
 function App() {
   return (
     <Router>
-    <div>
-      <Navbar />
-      <Searchbar />
-    </div>
-  </Router>
-    
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Searchbar />
+          <Table />
+        </Wrapper>
+      </div>
+    </Router>
+
   );
 }
 
