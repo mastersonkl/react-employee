20af597cae67d0b5f9ab6c088911a0779ba4389f
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-23 11:46:47 -0400

    Added main.js

---
+++ src/pages/main.js
@@ -0,0 +1,13 @@
+import React, { Component, useState, useEffect } from "react"
+import Searchbar from "../components/searchbar"
+import Table from "../components/table"
+
+
+function Main() {
+    const [employee, setEmployee] = useState({})
+    const [employees, setEmployees] = useState([])
+    const [employeeIndex, setEmployeeIndex] = useState(0)
+
+}
+
+export default Main
